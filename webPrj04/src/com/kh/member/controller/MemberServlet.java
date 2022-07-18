c51475b129085c8f7a68dd1784599061cb8d1ee1
lee990103@naver.com <lee990103@naver.com>
2022-07-18 15:54:43 +0900

    no message

--- webPrj04/src/com/kh/member/controller/MemberServlet.java
+++ webPrj04/src/com/kh/member/controller/MemberServlet.java
@@ -1,4 +1,4 @@
-package webPrj04;
+package com.kh.member.controller;
 
 import java.io.IOException;
 
@@ -8,13 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//webPrj04 중에서 /member/join 이런 요청이 들어오면 이 서블릿이 담당을 하겠다.
 @WebServlet(urlPatterns = "/member/join")
 public class MemberServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("MemberServlet > service 호출됨");
-		
 	}
-	
 }
