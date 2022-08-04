a757def20f2eaf0ff3f496ea167d1b1ab055d119
AREUM\lee99 <lee990103@naver.com>
2022-08-04 22:24:06 +0900

    회원 탈퇴 완성

--- webPrj99semi/src/com/kh/member/controller/MemberQuitController.java
+++ webPrj99semi/src/com/kh/member/controller/MemberQuitController.java
@@ -33,10 +33,23 @@ public class MemberQuitController extends HttpServlet {
 			//우선 로그아웃
 			req.getSession().invalidate();
 			
+			/*
+			 * 포워딩 리다이렉트
+
+				url 바뀐다
+				클라이언트가 한 번 더 요청이 온다
+				다음 화면이 req에 있는 데이터를 필요로 하는가 ? -> 포워딩 필요
+				내가 지금 들고 있는 데이터를 req에 담고 있어야 하면 포워딩, 그게 아니라면 redirect
+
+			 */
 		
 			req.setAttribute("Msg", "회원 탈퇴 성공!");
 			req.getRequestDispatcher("/views/common/header.jsp").forward(req, resp);
 			
+			
+			//이거 안됨..........
+//			req.getSession().setAttribute("alertMsg", "회원 탈퇴 성공!"); 
+//			resp.sendRedirect("/views/common/header.jsp"); 
 			//메인으로 가기 -> redirect -> 로그아웃 했으므로 클라이언트에게 다시 요청을 보내라고 하기 위해 -> 이걸로 해봤는데 안됨..
 //			resp.sendRedirect("/views/common/header.jsp");
 			
